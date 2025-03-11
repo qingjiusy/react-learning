cf39cdcea9ff90bded7ed103fb14ecf395b68ee7
sy <101882798+qingjiusy@users.noreply.github.com>
2025-03-11 23:22:29 +0900

    computation based on month

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,5 +1,93 @@
-function Month() {
-    return <div>this is Month</div>;
-  }
+import { NavBar, DatePicker } from 'antd-mobile'
+import './index.scss'
+import { useMemo, useState } from 'react'
+import classNames from 'classnames'
+import dayjs from 'dayjs'
+import { useSelector } from 'react-redux'
+import _ from 'lodash'
+
+const Month = () => {
+  //按月做数据的分组
+  const billList = useSelector(state => state.bill.billList)
+  const monthGroup = useMemo(() => {
+    //return出去计算之后的值
+    return _.groupBy(billList, (item)=>dayjs(item.date).format('YYYY-MM'))
+  },[billList])
+
+  //控制弹窗的打开和关闭
+  const [dateVisible, setDateVisible] = useState(false)
+
+  //控制时间显示
+  const [currentDate, setCurrentDate] = useState(()=>{
+    return dayjs(new Date()).format('YYYY-MM')
+  })
+
+  const [currentMonthList, setMonthList] = useState([])
   
-  export default Month;
+  const monthResult = useMemo(()=>{
+    //支出/收入/结余
+    const pay = currentMonthList.filter(item=>item.type==='pay').reduce((a, c)=>a + c.money, 0)
+    const income = currentMonthList.filter(item=>item.type==='income').reduce((a, c)=>a + c.money, 0)
+    return {
+      pay,
+      income,
+      total: pay + income
+    }
+  }, [currentMonthList])
+
+  //确认回调
+  function onConfirm(date) {
+    setDateVisible(false)
+    console.log(date)
+    const formatDate = dayjs(date).format('YYYY-MM')
+    setMonthList(monthGroup[formatDate])
+    setCurrentDate(formatDate)
+  }
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={()=>setDateVisible(true)}>
+            <span className="text">
+              {currentDate + ''}月账单
+            </span>
+            {/* {根据弹窗打开状态控制expand类名是否存在} */}
+            <span className={classNames('arrow', dateVisible && 'expand')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className="item">
+              <span className="money">{monthResult.pay.toFixed(2)}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{monthResult.income.toFixed(2)}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{monthResult.total.toFixed(2)}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            onCancel={() => setDateVisible(false)}
+            onConfirm={onConfirm}
+            onClose={() => setDateVisible(false)}
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dateVisible}
+            max={new Date()}
+          />
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default Month

8eb5414919d7de1b3869a9b2398dda96c2e5093d
sy <wsy2781064355@gmail.com>
2025-03-19 18:09:11 +0900

    使用redux维护token数据

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,23 @@
 import './index.scss'
 import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
+import { useDispatch } from 'react-redux'
+import { fetchLogin } from '@/store/modules/user'
+import { useNavigate } from 'react-router-dom'
+import { message } from 'antd'
 
 const Login = () => {
-  function onFinish(values){
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  async function onFinish(values){
     console.log(values)
+    //触发异步action fetchLogin
+    await dispatch(fetchLogin(values))
+    //跳转到首页
+    navigate('/')
+    //提示用户
+    message.success('登录成功')
   }
 
   return (
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+//统一中转工具模块函数
+//方便导出的可读性
+
+import { request } from "./request";
+
+export {
+    request
+}
